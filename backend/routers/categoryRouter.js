a5f5d671e36aa47be6c499302f12e213b39c9ea3
Karolis Valatka <karolisvalatka0@gmail.com>
2022-04-20 10:34:08 +0300

    CRUD done for category router.

--- backend/routers/categoryRouter.js
+++ backend/routers/categoryRouter.js
@@ -10,6 +10,14 @@ const router = express.Router();
       }
       res.send(category);
   })
+
+  router.get(`/:id`, async (req, res) => {
+    const category = await Category.findById(req.params.id);
+    if(!category){
+      res.status(500).json({success: false, error: 'Category not found'});
+    }
+    res.status(200).send(category);
+  })
   
   router.post(`/`, async (req, res) => {
     const newCategory = new Category(req.body);
@@ -24,5 +32,34 @@ const router = express.Router();
       }
     });
   })
+
+  router.put(`/:id`, async (req, res) => {
+    const category = await Category.findByIdAndUpdate(req.params.id, req.body, {new: true});
+    if(!category){
+      res.status(500).json({success: false, error: 'Category not found'});
+    }
+    res.status(200).send(category);
+  })
+
+  router.delete(`/:id`,  (req, res) => {
+    Category.findByIdAndDelete(req.params.id).then(category => {
+      if(category){
+        return res.status(200).json({
+          success: true,
+          category: category
+          });
+      } else {
+        return res.status(404).json({
+          error: 'Category not found',
+          success: false
+        })
+      }
+  }).catch(err => {
+    return res.status(500).json({
+      error: err,
+      success: false
+    })
+  })
+})
 
   module.exports = router;
